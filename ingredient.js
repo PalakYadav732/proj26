dbbf107149f3074e8dda5851b4db6fc78d4e821e
PalakYadav732 <82311008+PalakYadav732@users.noreply.github.com>
2021-11-01 18:08:20 +0530

    Create ingredient.js

---
+++ ingredient.js
@@ -0,0 +1,26 @@
+1 cup coriander
+½ cup mint
+2 tbsp roasted gram dal
+4 chilli
+2 inch ginger
+4 cloves garlic
+2 tbsp lemon juice
+½ tsp cumin powder
+1 tsp chaat masala
+½ tsp aamchur
+1 tsp sugar
+½ tsp salt
+3 tbsp water
+for club sandwich:
+3 slice bread (white / brown)
+butter
+4 slice potato (boiled)
+4 slice tomato
+ cheese sandwich:
+2 slice bread (white / brown)
+butter
+4 slice potato (boiled)
+6 slice cucumber
+4 slice tomato
+5 slice onion
+2 tbsp cheese (grated)
